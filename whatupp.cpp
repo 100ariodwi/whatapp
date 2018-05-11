e15fc13afe6d292bb4f31bc9e05a834c7160859a
100ariodwi <39194951+100ariodwi@users.noreply.github.com>
2018-05-11 23:23:20 +0700

    ario dwi laskosono

---
+++ whatupp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class WhatApp{
+    private:
+	string kontak
+	
+	public
+	wa(string kontak, string pesan):KirimPesan(teks){
+    this->kontak = kontak;
+	this->pesan = Pesan;
+    }
+
+    void Pesan(){
+	cout<< "kontak WhatApp"<<endl;
+	cout<< "Teks\t\t"<<kontak<<endl;
+	cout<< "Pesan\t\t"<<pesan<<endl;
+
+};
